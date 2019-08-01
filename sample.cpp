7bf8b3f65b4425a611c631b5c802026295a7a11a
suyashsonawane <suyashsonawane007@gmail.com>
2019-08-01 12:10:17 +0530

    adding

---
+++ sample.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+#include <string.h> 
+using namespace std;
+  
+void add(float a)
+{
+	cout<<"\n Square = "<<a*a;
+}
+int main() 
+{
+/*	int i;
+	float j;
+	i=(int)3.5; 
+	j=(float)3;
+*/	add(3);
+	add(3.5);
+//	cout<<"\nI = "<<i;
+//	cout<<"\nJ = "<<j;
+    return 0; 
+}
+
